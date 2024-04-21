7c1f6ee985ec8da4da8316cb30cd11dbfd6ed242
pablo1479 <130510225+pablo1479@users.noreply.github.com>
2024-04-21 16:05:27 -0700

    updated css

--- esports/registration/forms.py
+++ esports/registration/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import Registrant
+from django.contrib.auth.forms import AuthenticationForm
 
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username', 'id': 'username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'id': 'password'}))
+    
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registrant
