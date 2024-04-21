91943c545ecafba38549f1aa235050ebdeeb418c
Alex Rivera <35387065+Alexriv@users.noreply.github.com>
2024-04-20 21:24:16 -0700

    navbar fixed

--- esports/registration/urls.py
+++ esports/registration/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('home/', views.index, name='home'),  # Empty path for the root of the site
-
+    path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
+    path('application/', views.application, name='application'),
+    path('stats/', views.stats, name='stats'),
+    path('contact/', views.contact, name='contact'),
+    path('merch/', views.merch, name='merch'),
+    path('stream/', views.stream, name='stream'),
 ]
 
