9f8c20f0ef97f0736a11e07fcaa4bc3a3592f03a
pablo1479 <130510225+pablo1479@users.noreply.github.com>
2024-04-20 19:21:53 -0700

    updated django

--- esports/registration/urls.py
+++ esports/registration/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
-    path('', views.home, name='home'),  # Empty path for the root of the site
+    path('register/', views.register, name='register'),
+    path('home/', views.home, name='home'),  # Empty path for the root of the site
 ]
