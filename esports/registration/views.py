7c024c0faf19665e2a05184c490236dbed8b6f55
Alex Rivera <35387065+Alexriv@users.noreply.github.com>
2024-04-20 21:51:38 -0700

    URL works

--- esports/registration/views.py
+++ esports/registration/views.py
@@ -12,22 +12,71 @@ def register(request):
     return render(request, 'registration/register.html', {'form': form})
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/about.html')
 
 def application(request):
-    return render(request, 'application.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/Application.html')
 
 def stats(request):
-    return render(request, 'stats.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/stats.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/contact.html')
 
 def merch(request):
-    return render(request, 'merch.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/merch.html')
 
 def stream(request):
-    return render(request, 'stream.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/success.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/stream.html')
