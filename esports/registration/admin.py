d57dd44111fa3663dc94dbd05276c5a07d64ac98
Alex Rivera <35387065+Alexriv@users.noreply.github.com>
2024-04-21 00:23:33 -0700

    Backend actions

--- esports/registration/admin.py
+++ esports/registration/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Registrant, Team, Game  # Import the models you want to register with the admin site
 
-# Register your models here.
+# Register your models with the admin site
+admin.site.register(Registrant)
+admin.site.register(Team)
+admin.site.register(Game)
+
+
--- esports/registration/forms.py
+++ esports/registration/forms.py
@@ -5,6 +5,5 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registrant
         fields = ['first_name', 'last_name', 'student_email', 'password', 'student_id', 'twitch']
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+
+
